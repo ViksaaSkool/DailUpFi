846250f23670b7f2c5d0945f4afeea9e4a3c9fab
Viktor Arsovski <victor.arsovski@gmail.com>
2018-12-20 23:25:41 +0100

    configuration

--- app/src/main/java/com/viksaa/dailupfi/app/MainActivity.kt
+++ app/src/main/java/com/viksaa/dailupfi/app/MainActivity.kt
@@ -5,10 +5,11 @@ import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
-
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,6 +19,8 @@ class MainActivity : AppCompatActivity() {
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
+
+            fab.also {  }
         }
     }
 
